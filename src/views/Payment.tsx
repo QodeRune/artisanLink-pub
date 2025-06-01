584c8e88bcfc33ab32abe77496603fdf8d2fbace
QodeRune <qoderune@gmail.com>
2025-06-01 23:40:58 +0100

    feat: payment

--- src/views/Payment.tsx
+++ src/views/Payment.tsx
@@ -1,5 +1,18 @@
 // src/views/Payment.tsx
 import type { FC } from "react"
+import { FormInput } from "@/components"
+
+const _promo = {
+  id: "prom_code",
+  name: "prom_code",
+  labelText: "Have a promo code?",
+  type: "text",
+  readOnly: false,
+  placeholder: "Referrer's code",
+  required: true,
+  minLength: 3,
+  helperText: "Please enter your Promo Code",
+}
 
 export const Payment: FC = () => {
   return (
@@ -7,7 +20,7 @@ export const Payment: FC = () => {
       <h1 className="section_heading">Payment</h1>
       <div className="grid-section product-section">
         <span className="product-item">
-          <img src="" alt="" className="product-image" />
+          <img src="" width="3rem" height="auto" alt="" className="product-image" />
           <span className="product-details">
             <h2 className="product-name">Career Tapestry Snap shot</h2>
             <p className="product-about">
@@ -24,17 +37,30 @@ export const Payment: FC = () => {
             </span>
           </span>
         </span>
-        <span className="prom-section">
-          <p>Have a promo code?</p>
-          <span className="promo-input">
-            <p>Promo code</p>
-            <p>Apply code</p>
-          </span>
+        <span className="promo-section">
+          {/* <p>Have a promo code?</p> */}
+          <form action="" className="promo-input">
+            <FormInput {..._promo} containerClassName="enter-promo" />
+            <input type="button" value="submit" className="form_input submit_button" />
+          </form>
         </span>
 
         <span className="order-summary-section">
           <h2>Order summary</h2>
+          <span className="summary-list">
+            <p className="summary-list-item">1 item(s)</p>
+            <p className="summary-list-item">$100.00</p>
+          </span>
+          <span className="summary-list">
+            <p className="summary-list-item">Discount</p>
+            <p className="summary-list-item">$10.00</p>
+          </span>
         </span>
+        <span className="summary-list">
+          <p className="summary-list-item">Total</p>
+          <p className="summary-list-item">$90.00</p>
+        </span>
+        <input type="button" value="Proceed TO Checkout" className="form_input submit_button" />
       </div>
     </section>
   )
