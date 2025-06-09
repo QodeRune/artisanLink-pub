a9c952e3f69287c4c00256dfe3e72fc7f94aa604
QodeRune <qoderune@gmail.com>
2025-06-09 09:39:57 +0100

    feat: article card overflow-y

--- src/views/Assessment.tsx
+++ src/views/Assessment.tsx
@@ -3,7 +3,9 @@ import { ArticleItemCard } from "@/components"
 
 export const AssessmentList: FC = () => {
   return (
-    <section className="grid-autofill">
+    <section className="grid-autofill scroll-y">
+      <ArticleItemCard />
+      <ArticleItemCard />
       <ArticleItemCard />
       <ArticleItemCard />
     </section>
