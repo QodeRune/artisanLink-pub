c75f12b2bd58cfbfe147c3d93a200a507bf269c0
QodeRune <qoderune@gmail.com>
2025-06-29 14:24:02 +0100

    feat: user badge popup

--- src/core/feature/user/UserProfileMenu.tsx
+++ src/core/feature/user/UserProfileMenu.tsx
@@ -1,20 +1,37 @@
 // src/components/user/UserProfileMenu.tsx
+import { RoutePaths } from "@/navigation"
+import { useAuthHook } from "@/store"
 import type { FC } from "react"
+import { useNavigate } from "react-router-dom"
 
 interface UserProfileMenuProps {
   onClose: () => void
 }
 
 export const UserProfileMenu: FC<UserProfileMenuProps> = ({ onClose }) => {
+  const navigate = useNavigate()
+  const { handleLogout } = useAuthHook()
+
+  const logout = () => {
+    handleLogout()
+    navigate(RoutePaths.LOG_OUT)
+    onClose()
+  }
+
+  const settings = () => {
+    navigate(RoutePaths.DASHBOARD_PROFILE)
+    onClose()
+  }
   return (
     <div className="user-menu-popup u-soft-shadow">
-      <button className="user-menu-item" onClick={onClose}>
-        Settings
+      {/* TODO:: Remove the disabled when support is enabled */}
+      <button className="user-menu-item btn-is-disabled" onClick={onClose}>
+        Get Support
+      </button>{" "}
+      <button className="user-menu-item" onClick={settings}>
+        Profile & Settings
       </button>
-      <button className="user-menu-item" onClick={onClose}>
-        Help
-      </button>
-      <button className="user-menu-item" onClick={onClose}>
+      <button className="user-menu-item" onClick={logout}>
         Log out
       </button>
     </div>
